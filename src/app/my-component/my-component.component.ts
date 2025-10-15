6e9a5baf320f230d28a99a5cef571121998ccf58
Aishwarya Kulkarni <st0763ak1@partner.bajajfinserv.in>
2025-10-15 11:56:16 +0530

    Initial Angular project

---
+++ src/app/my-component/my-component.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-component',
+  templateUrl: './my-component.component.html',
+  styleUrls: ['./my-component.component.css']
+})
+export class MyComponentComponent {
+  num1: number = 0;
+  num2: number = 0;
+  result: number | null = null;
+
+  calculate(operator: string): void {
+    switch (operator) {
+      case '+':
+        this.result = this.num1 + this.num2;
+        break;
+      case '-':
+        this.result = this.num1 - this.num2;
+        break;
+      case '*':
+        this.result = this.num1 * this.num2;
+        break;
+      case '/':
+        this.result = this.num2 !== 0 ? this.num1 / this.num2 : NaN;
+        break;
+      default:
+        this.result = null;
+    }
+  }
+}
+
+
